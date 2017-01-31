5c47da1a5aa2b3866e051da39378c6a992e7f517
Deep S Randhawa <deepsaranr@gmail.com>
2017-01-30 23:01:16 -0500

    Completed HW Assignment

---
+++ flicks/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  flicks
+//
+//  Created by Deep S Randhawa on 1/30/17.
+//  Copyright © 2017 Deep S Randhawa. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
